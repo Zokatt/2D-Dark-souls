e084c36169dff7062d7e80d2a2f7d021ab93681c
Hoffe <christian.hoffmann.thomsen@gmail.com>
2020-11-23 11:43:18 +0100

    Collision
    Collision ADD

--- GameWorld.cs
+++ GameWorld.cs
@@ -8,6 +8,7 @@ namespace _2D_Dark_souls
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D collisionTexture;
 
         public GameWorld()
         {
@@ -21,14 +22,13 @@ namespace _2D_Dark_souls
             // TODO: Add your initialization logic here
 
             base.Initialize();
-            // HEY! - Nikolaj
-            //Ello - Hoffe
-            // hei - Tai
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            collisionTexture = Content.Load<Texture2D>("Pixel");
 
             // TODO: use this.Content to load your game content here
 
@@ -43,6 +43,20 @@ namespace _2D_Dark_souls
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+        }
+
+        private void DrawCollisionBox(GameObject go)
+        {
+            //Der laves en streg med tykkelsen 1 for hver side af Collision.
+            Rectangle topLine = new Rectangle(go.Collision.X, go.Collision.Y, go.Collision.Width, 1);
+            Rectangle bottomLine = new Rectangle(go.Collision.X, go.Collision.Y + go.Collision.Height, go.Collision.Width, 1);
+            Rectangle rightLine = new Rectangle(go.Collision.X + go.Collision.Width, go.Collision.Y, 1, go.Collision.Height);
+            Rectangle leftLine = new Rectangle(go.Collision.X, go.Collision.Y, 1, go.Collision.Height);
+            //Der tegnes en streg med tykkelsen 1 for hver side af Collision med collsionTexture med farven rød.
+            _spriteBatch.Draw(collisionTexture, topLine, Color.Red);
+            _spriteBatch.Draw(collisionTexture, bottomLine, Color.Red);
+            _spriteBatch.Draw(collisionTexture, rightLine, Color.Red);
+            _spriteBatch.Draw(collisionTexture, leftLine, Color.Red);
         }
 
         protected override void Draw(GameTime gameTime)
--- Player.cs
+++ Player.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _2D_Dark_souls
 {
-    class Player : GameObject
-{
-}
+    internal class Player : GameObject
+    {
+        public override void LoadContent(ContentManager contentManager)
+        {
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+        }
+
+        public override void Update(GameTime gametime)
+        {
+        }
+    }
 }
