49204245155525fd9ec5578b37f40867d938fb79
Nikolaj Hornung Larsen <10714@student.eadania.dk>
2020-11-23 11:52:50 +0100

    idfk?

--- Enemy.cs
+++ Enemy.cs
@@ -1,23 +1,10 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _2D_Dark_souls
 {
-    internal class Enemy : GameObject
-    {
-        public override void LoadContent(ContentManager contentManager)
-        {
-        }
-
-        public override void OnCollision(GameObject other)
-        {
-        }
-
-        public override void Update(GameTime gametime)
-        {
-        }
-    }
+    class Enemy : GameObject
+{
+}
 }
