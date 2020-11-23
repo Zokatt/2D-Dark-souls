fa53311c25ed9fa00da77933a0e73ae1c1c28e1e
Nikolaj Hornung Larsen <10714@student.eadania.dk>
2020-11-23 10:14:59 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2D_Dark_souls
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameWorld())
+                game.Run();
+        }
+    }
+}
