e3e9761602ab1e807842544d344be0c9f4da447e
xTaig4 <xTaig4@users.noreply.github.com>
2020-11-23 12:32:49 +0100

    Added field, constructor, method
    Added fields from "klassediagram", enemy constructor and empty EnemyMovement method

--- Enemy.cs
+++ Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,12 @@ namespace _2D_Dark_souls
 {
     internal class Enemy : GameObject
     {
+        private int hp;
+        private Vector2 offset;
+        private int speed;
+        private int attackTimer;
+        private int dmg;
+
         public override void LoadContent(ContentManager contentManager)
         {
         }
@@ -18,6 +25,17 @@ namespace _2D_Dark_souls
 
         public override void Update(GameTime gametime)
         {
+        }
+
+        protected Enemy(Vector2 position, Texture2D sprite)
+        {
+            this.position = position;
+            this.sprite = sprite;
+        }
+
+        private void Almovement()
+        {
+
         }
     }
 }
