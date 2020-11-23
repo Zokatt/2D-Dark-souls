718c57ad58a80c1f2a2cfa8a4d7e6148c4905d95
JohnnySii <johnnysii39@gmail.com>
2020-11-23 15:55:01 +0100

    sprite added

--- Enemy.cs
+++ Enemy.cs
@@ -17,6 +17,7 @@ namespace _2D_Dark_souls
 
         public override void LoadContent(ContentManager contentManager)
         {
+            sprite = contentManager.Load<Texture2D>("Jimmy");
         }
 
         public override void OnCollision(GameObject other)
