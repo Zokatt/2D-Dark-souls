8aacd1aed6d0319c83af0117a7a22362a4fc988d
Hoffe <christian.hoffmann.thomsen@gmail.com>
2020-11-23 11:55:09 +0100

    Collision FIX
    Fix

--- AttackBox.cs
+++ AttackBox.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _2D_Dark_souls
 {
-    class AttackBox :GameObject
-{
-}
+    internal class AttackBox : GameObject
+    {
+        public override void LoadContent(ContentManager contentManager)
+        {
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+        }
+
+        public override void Update(GameTime gametime)
+        {
+        }
+    }
 }
--- Enviroment.cs
+++ Enviroment.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _2D_Dark_souls
 {
-    class Enviroment : GameObject
-{
-}
+    internal class Enviroment : GameObject
+    {
+        public override void LoadContent(ContentManager contentManager)
+        {
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+        }
+
+        public override void Update(GameTime gametime)
+        {
+        }
+    }
 }
