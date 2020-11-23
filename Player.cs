33c03807367855039f8fa9fd542a6c9952c6f0ed
Hoffe <christian.hoffmann.thomsen@gmail.com>
2020-11-23 12:09:28 +0100

    Player update

--- Player.cs
+++ Player.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +10,12 @@ namespace _2D_Dark_souls
 {
     internal class Player : GameObject
     {
+        public Player(Vector2 position, Texture2D sprite)
+        {
+            position = this.position;
+            sprite = this.sprite;
+        }
+
         public override void LoadContent(ContentManager contentManager)
         {
         }
@@ -18,6 +26,35 @@ namespace _2D_Dark_souls
 
         public override void Update(GameTime gametime)
         {
+            HandleInput();
+        }
+
+        private void HandleInput()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Up))
+            {
+                position.Y++;
+            }
+            else if (state.IsKeyDown(Keys.Down))
+            {
+                position.Y--;
+            }
+            else if (state.IsKeyDown(Keys.Left))
+            {
+                position.X--;
+            }
+            else if (state.IsKeyDown(Keys.Right))
+            {
+                position.X++;
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            color = Color.Gray;
         }
     }
 }
