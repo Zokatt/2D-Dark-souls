4e00513e95c9df2821e81e57d80509f3b25cd4ef
xTaig4 <xTaig4@users.noreply.github.com>
2020-11-23 19:35:06 +0100

    WIP
    still in work

--- Enviroment.cs
+++ Enviroment.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,12 +9,24 @@ namespace _2D_Dark_souls
 {
     internal class Enviroment : GameObject
     {
+        public Vector2 Size { get; set; }
+        
+        public override Rectangle Collision
+        {
+            get { return new Rectangle(position.ToPoint(), (Size).ToPoint()); }
+        }
+
         public override void LoadContent(ContentManager contentManager)
         {
+            contentManager.Load<Texture2D>("StoneGround");
         }
 
         public override void OnCollision(GameObject other)
         {
+            if(other is Enviroment)
+            {
+                color = Color.Yellow;
+            }
         }
 
         public override void Update(GameTime gametime)
