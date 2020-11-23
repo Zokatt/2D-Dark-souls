d74c066cb878b8004d4e7e89dbdb4f2d3c2a2273
Nikolaj Hornung Larsen <10714@student.eadania.dk>
2020-11-23 11:55:44 +0100

    Fixed errors

--- Enviroment.cs
+++ Enviroment.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _2D_Dark_souls
 {
     class Enviroment : GameObject
-{
-}
+    {
+        public override void LoadContent(ContentManager contentManager)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnCollision(GameObject other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gametime)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
