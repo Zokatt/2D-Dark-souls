e933183a05cd9ef874f9a1af05aefd7465608544
Hoffe <christian.hoffmann.thomsen@gmail.com>
2020-11-23 13:48:51 +0100

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,8 +9,14 @@ namespace _2D_Dark_souls
 {
     internal class Enemy : GameObject
     {
+        public Enemy(Vector2 position)
+        {
+            position = this.position;
+        }
+
         public override void LoadContent(ContentManager contentManager)
         {
+            sprite = contentManager.Load<Texture2D>("Jimmy");
         }
 
         public override void OnCollision(GameObject other)
@@ -18,6 +25,12 @@ namespace _2D_Dark_souls
 
         public override void Update(GameTime gametime)
         {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            color = Color.Black;
         }
     }
 }
