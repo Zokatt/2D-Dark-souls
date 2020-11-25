330ce3f403d882c94964acc3d222d5e417a99cac
Nikolaj Hornung Larsen <10714@student.eadania.dk>
2020-11-25 13:57:36 +0100

    fixed attackbox

--- AttackBox.cs
+++ AttackBox.cs
@@ -1,24 +1,55 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _2D_Dark_souls
 {
-    internal class AttackBox : GameObject
+    public class AttackBox : GameObject
     {
+        //private string chosenSprite;
+        private int _spriteWidth;
+
+        public AttackBox(Texture2D chosenSprite, Vector2 position, int stretch)
+        {
+            _spriteWidth = stretch;
+            sprite = chosenSprite;
+            this.position = position;
+        }
+
         public override void LoadContent(ContentManager contentManager)
         {
+            //sprite = contentManager.Load<Texture2D>(chosenSprite);
         }
 
         public override void OnCollision(GameObject other)
         {
-
         }
 
         public override void Update(GameTime gametime)
         {
+        }
+
+        public override Rectangle Collision
+        {
+            get
+            {
+                return new Rectangle(
+                       (int)position.X,
+                       (int)position.Y,
+                       (int)this._spriteWidth,
+                       (int)this.sprite.Height
+                   );
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite,
+                new Rectangle((int)position.X, (int)position.Y, _spriteWidth, sprite.Height),
+                new Rectangle(1, 1, sprite.Width, sprite.Height), color);
         }
     }
 }
